67afc211d221c1e3d8a1ee57579f58a8ed792857
Eddie-He-090 <2021022249@m.scnu.edu.cn>
2022-04-13 22:12:16 +0800

    change package.json

--- src/services/Service.js
+++ src/services/Service.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const jsonplaceholderApiClient = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/Eddie-He-090/favicons-json',
+    baseURL: 'https://my-json-server.typicode.com/Eddie-He-090/favicons-json',
     withCredentials: false,
     headers: {
         Accept: 'application/json',
