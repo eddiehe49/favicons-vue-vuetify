daa21f0ed72fe30d80670ed9b463623b896cfe96
Eddie-He-090 <2021022249@m.scnu.edu.cn>
2022-04-17 09:16:09 +0800

    Use .env

--- src/services/Service.js
+++ src/services/Service.js
@@ -13,7 +13,7 @@ const jsonbinApiClient = axios.create({
     baseURL: "https://api.jsonbin.io/v3/b/6247a881d96a510f028f6bfd",
     withCredentials: false,
     headers: {
-        "X-Master-Key": "$2b$10$Dr3C03m96HY3V014.oTOf.kh/veUr7PF2tLr7FK/B9v03pstYPCSO",
+        "X-Master-Key": "$2b$10$Dr3C03m96HY3V014" + process.env.VUE_APP_X_Master_Key,
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
@@ -22,7 +22,7 @@ const kratesApiClient = axios.create({
     baseURL: "https://krat.es/258439960bc7d64a1353",
     withCredentials: false,
     headers: {
-        "x-api-key": "3c46750c-1795-431e-9bc0-fcfb3a43c856",
+        "x-api-key": process.env.VUE_APP_x_api_key,
         Accept: 'application/json',
         'Content-Type': 'application/json'
     }
